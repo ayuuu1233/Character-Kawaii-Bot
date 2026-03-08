c07f60b81baa72e79beba19959ddce51fa9e29ba
ᴀʏᴜsʜʜ ᴘᴀʀᴋ <maharanachinmaya79@gmail.com>
2026-03-08 18:24:11 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -1,18 +1,10 @@
-import asyncio
-
-# -------- EVENT LOOP FIX (IMPORTANT FOR PYTHON 3.14) --------
-try:
-    asyncio.get_event_loop()
-except RuntimeError:
-    asyncio.set_event_loop(asyncio.new_event_loop())
-
 import logging
 import os
+import pytz  
 from pyrogram import Client
-from telegram.ext import Application
+from telegram.ext import ApplicationBuilder
 from motor.motor_asyncio import AsyncIOMotorClient
-
-# ---------------- LOGGING ---------------- #
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
@@ -21,15 +13,13 @@ logging.basicConfig(
 )
 
 logging.getLogger("apscheduler").setLevel(logging.ERROR)
-logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger('httpx').setLevel(logging.WARNING)
 logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
-
 LOGGER = logging.getLogger(__name__)
-
-# ---------------- CONFIG ---------------- #
 
 from shivu.config import Development as Config
 
+# --- AAPKI PURANI VARIABLES (SAME TO SAME) ---
 api_id = Config.api_id
 api_hash = Config.api_hash
 TOKEN = Config.TOKEN
@@ -44,57 +34,53 @@ sudo_users = Config.sudo_users
 OWNER_ID = Config.OWNER_ID
 JOINLOGS = Config.JOINLOGS
 LEAVELOGS = Config.LEAVELOGS
-
+owner = Config.OWNER_ID
 GRADE4 = Config.GRADE4
 GRADE3 = Config.GRADE3
 GRADE2 = Config.GRADE2
 GRADE1 = Config.GRADE1
 SPECIALGRADE = Config.SPECIALGRADE
-
-Genin = Config.Genin
-Chunin = Config.Chunin
-Jonin = Config.Jonin
-Hokage = Config.Hokage
+Genin= Config.Genin
+Chunin= Config.Chunin
+Jonin= Config.Jonin
+Hokage= Config.Hokage
 Akatsuki = Config.Akatsuki
 Princess = Config.Princess
 
-# ---------------- TELEGRAM CLIENTS ---------------- #
+# --- NEW FIX CODE (TIMEZONE ERROR KILLER) ---
+# Forced UTC timezone taaki Hugging Face crash na ho
+scheduler = AsyncIOScheduler(timezone=pytz.utc)
+application = ApplicationBuilder().token(TOKEN).build()
 
-application = Application.builder().token(TOKEN).build()
-
-shivuu = Client(
-    "Shivu",
-    api_id=api_id,
-    api_hash=api_hash,
-    bot_token=TOKEN
-)
-
-# ---------------- DATABASE ---------------- #
-
+# --- AAPKI PURANI COLLECTIONS (SAME TO SAME) ---
+shivuu = Client("Shivu", api_id, api_hash, bot_token=TOKEN)
 lol = AsyncIOMotorClient(mongo_url)
-
-db = lol["Character_catcher"]
-
-set_on_data = db["set_on_data"]
-refeer_collection = db["refeer_collection"]
-set_off_data = db["set_off_data"]
-
-collection = db["anime_characters_lol"]
-
+db = lol['Character_catcher']
+set_on_data = db['set_on_data']
+refeer_collection = db['refeer_collection']
+set_off_data = db['set_off_data']
+collection = db['anime_characters_lol']
 safari_cooldown_collection = db["safari_cooldown"]
 safari_users_collection = db["safari_users_collection"]
-
-sudo_users_collection = db["sudo_users_collection"]
-
-user_totals_collection = db["user_totals_lmaoooo"]
+sudo_users_collection= db["sudo_users_collection"]
+user_totals_collection = db['user_totals_lmaoooo']
 user_collection = db["user_collection_lmaoooo"]
-
-group_user_totals_collection = db["group_user_totalsssssss"]
-top_global_groups_collection = db["top_global_groups"]
-
-pm_users = db["total_pm_users"]
-registered_users = db["registered_users"]
-
-backup_collection = db["backup_collection"]
+group_user_totals_collection = db['group_user_totalsssssss']
+top_global_groups_collection = db['top_global_groups']
+pm_users = db['total_pm_users']
+registered_users = db['registered_users']
+backup_collection = db['backup_collection']
 settings_collection = db["settings"]
-event_collection = db["event_collection"]
+event_collection = db['event_collection']
+
+async def get_served_chat():
+    served_chats = []
+    async for chat in top_global_groups_collection.find():
+        served_chats.append(chat['id'])
+    return served_chats
+
+async def get_pm_users():
+    pm_users_list = []
+    async for user in pm_users.find():
+        pm_users_list.append(user['id'])
+    return pm_users_list
